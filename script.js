62ab8de27e5a66c3c62bfdfef021e080c693c155
Deucalio <hjamad89@gmail.com>
2022-10-01 23:41:07 +0500

    complete game

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ Array.prototype.insert = function (index, ...items) {
     this.splice(index, 0, ...items);
 };
 
-const cells = [...document.querySelectorAll(".card > button")]
+const cells = [...document.querySelectorAll(".card > button")];
+const overlay = document.querySelector("#overlay");
+const modal = document.querySelector("#modal")
+const restartBtn = document.querySelector("#restart")
 
-
-const User = {};
-const Computer = {};
+let User = {};
+let Computer = {};
 let gameStart = false;
+
+restartBtn.addEventListener("click", () => {
+    restart();
+})
 
 // If the user clicks the marker start the game
 markers.forEach(marker => marker.addEventListener("click", () => {
@@ -29,25 +35,62 @@ markers.forEach(marker => marker.addEventListener("click", () => {
 
     User['marker'] = marker.textContent
     Computer['marker'] = (User.marker === "O") ? "X" : "O"
-}))
 
-cells.forEach(btn => btn.addEventListener("click", () => {
-    // If user marker is O then it's user's turn
-    if (User.marker === "O") {
-        btn.textContent = User.marker;
-        let [row, col] = btn.dataset.rowcol.split("");
-        gameBoard[row - 1].splice(col - 1, 1, User.marker)
+    // if user marker is X then computer takes the first turn
+    if (User['marker'] === "X") {
         let computerChoice = ComputerTurnGen()
         cells[computerChoice].textContent = Computer.marker;
 
         // for Computer (also disable the button where computer marks)
-        [row, col] = cells[computerChoice].dataset.rowcol.split("");
+        let [row, col] = cells[computerChoice].dataset.rowcol.split("");
         cells[computerChoice].disabled = true;
         // console.log("COMPUTER", row,col)
         gameBoard[row - 1].splice(col - 1, 1, Computer.marker)
 
+
+
     }
 }))
+
+cells.forEach(btn => btn.addEventListener("click", () => {
+    if (gameStart != false) {
+        // If user marker is O then it's user's turn
+        if (User.marker === "O") {
+            if (btn.textContent !== User.marker) { //if user doesn't click the same button again
+                btn.textContent = User.marker;
+                let [row, col] = btn.dataset.rowcol.split("");
+                gameBoard[row - 1].splice(col - 1, 1, User.marker)
+                let computerChoice = ComputerTurnGen()
+                cells[computerChoice].textContent = Computer.marker;
+
+                // for Computer (also disable the button where computer marks)
+                [row, col] = cells[computerChoice].dataset.rowcol.split("");
+                cells[computerChoice].disabled = true;
+                // console.log("COMPUTER", row,col)
+                gameBoard[row - 1].splice(col - 1, 1, Computer.marker)
+            }
+        } else {
+            btn.textContent = User.marker;
+            let [row, col] = btn.dataset.rowcol.split("");
+            gameBoard[row - 1].splice(col - 1, 1, User.marker)
+
+
+            let computerChoice = ComputerTurnGen()
+            cells[computerChoice].textContent = Computer.marker;
+
+            // for Computer (also disable the button where computer marks)
+            [row, col] = cells[computerChoice].dataset.rowcol.split("");
+            cells[computerChoice].disabled = true;
+            // console.log("COMPUTER", row,col)
+            gameBoard[row - 1].splice(col - 1, 1, Computer.marker)
+
+        }
+
+    }
+
+}))
+
+
 
 const ComputerTurnGen = () => {
 
@@ -64,6 +107,24 @@ const ComputerTurnGen = () => {
     }
 }
 
+function openModal(modal) {
+    if (modal == null) {
+        return
+    }
+    modal.classList.add("active")
+    overlay.classList.add("active")
+
+
+}
+
+function closeModal(closeBtn) {
+    if (closeBtn == null) {
+        return
+    }
+    closeBtn.classList.remove("active")
+    overlay.classList.remove("active")
+}
+
 // algorithm for determining the winner of tic tac toe
 // For single every move:
 //    checkDiagonals()
@@ -73,27 +134,56 @@ const ComputerTurnGen = () => {
 setInterval(() => {
     // check horizontals;
     if ((gameBoard[0][0] == gameBoard[0][1]) && (gameBoard[0][1] == gameBoard[0][2])) {
-        alert(gameBoard[0][0] + "WON")
+        gameOver(gameBoard[0][0]);
     } else if ((gameBoard[1][0] == gameBoard[1][1]) && (gameBoard[1][1] == gameBoard[1][2])) {
-        alert(gameBoard[1][0] + "WON")
+        gameOver(gameBoard[1][0]);
     } else if ((gameBoard[2][0] == gameBoard[2][1]) && (gameBoard[2][1] == gameBoard[2][2])) {
-        alert(gameBoard[2][0] + "WON")
+        gameOver(gameBoard[2][0]);
     }
 
     // check diagonally
     else if ((gameBoard[0][0] == gameBoard[1][1]) && (gameBoard[1][1] == gameBoard[2][2])) {
-        alert(gameBoard[0][0] + " " + "WON")
+        gameOver(gameBoard[0][0]);
     } else if ((gameBoard[0][2] == gameBoard[1][1]) && (gameBoard[1][1] == gameBoard[2][0])) {
-        alert(gameBoard[0][2] + " " + "WON")
+        gameOver(gameBoard[0][2]);
     }
 
     // check verticals;
     else if ((gameBoard[0][0] == gameBoard[1][0]) && (gameBoard[1][0] == gameBoard[2][0])) {
-        alert(gameBoard[0][0] + " " + "WON")
+        gameOver(gameBoard[0][0]);
     } else if ((gameBoard[0][1] == gameBoard[1][1]) && (gameBoard[1][1] == gameBoard[2][1])) {
-        alert(gameBoard[0][1] + " " + "WON")
+        gameOver(gameBoard[0][1]);
     } else if ((gameBoard[0][2] == gameBoard[1][2]) && (gameBoard[1][2] == gameBoard[2][2])) {
-        alert(gameBoard[0][2] + " " + "WON")
+        gameOver(gameBoard[0][2]);
     }
 
 }, 500)
+
+const gameOver = (winnerMarker) => {
+    overlay.dataset.content = winnerMarker + " " + "WON";
+    gameStart = false;
+    openModal(modal);
+    cells.forEach(btn => btn.disabled = true)
+    setTimeout(() => {
+        closeModal(modal)
+        // disable all buttons
+    }, 3000);
+}
+
+const restart = () => {
+    gameBoard = [
+        [1, -1, 0],
+        [0, 2, 4],
+        [5, 2, 1]
+    ]
+    gameStart = false;
+    markers.forEach(marker => {
+        marker.classList.remove("active")
+        marker.disabled = false;
+    })
+    cells.forEach(cell => {
+        cell.textContent = ""
+        cell.disabled = false;
+    });
+
+}
